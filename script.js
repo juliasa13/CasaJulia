2011d6397f5fe937a2a20ac4f9a4341909f70408
juliasa13 <juliasbaiao@gmail.com>
2024-12-08 23:05:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const products = [
         name: "Sofá 2 Lugares",
         description: "Confortável e estiloso.",
         price: 1200,
-        image: "https://postimg.cc/fJ4yjdcz",
+        image: "https://i.postimg.cc/fJ4yjdcz/sofa.jpg",
         link: "https://mpago.la/2nnsbVJ"
     },
     {
@@ -16,7 +16,7 @@ const products = [
         name: "Geladeira Frost Free",
         description: "Capacidade de 400L.",
         price: 2500,
-        image: "https://postimg.cc/bGMpYXLy",
+        image: "https://i.postimg.cc/bGMpYXLy/geladeira.jpg",
         link: "https://mpago.la/1jM1jRz"
     },
     {
@@ -24,7 +24,7 @@ const products = [
         name: "Jogo de Panelas",
         description: "Conjunto de 5 peças antiaderentes.",
         price: 300,
-        image: "https://postimg.cc/9z9FMfvv",
+        image: "https://i.postimg.cc/9z9FMfvv/panelas.jpg",
         link: "https://mpago.la/15gTR9w"
     },
     {
@@ -32,7 +32,7 @@ const products = [
         name: "Cama Queen",
         description: "Espaçosa e macia.",
         price: 1500,
-        image: "https://postimg.cc/tYHRVjb7",
+        image: "https://i.postimg.cc/tYHRVjb7/cama.jpg",
         link: "https://mpago.la/27HKrJA"
     },
     {
@@ -40,88 +40,8 @@ const products = [
         name: "Mesa de Jantar",
         description: "Para refeições deliciosas.",
         price: 900,
-        image: "https://postimg.cc/QKZD5pRv",
+        image: "https://i.postimg.cc/QKZD5pRv/mesa.jpg",
         link: "https://mpago.la/1RoDpT7"
-    },
-    {
-        id: 6,
-        name: "Cadeiras",
-        description: "Conjunto de 4 cadeiras confortáveis.",
-        price: 700,
-        image: "https://postimg.cc/QKZD5pRv",
-        link: "https://mpago.la/15sD4gZ"
-    },
-    {
-        id: 7,
-        name: "Talheres",
-        description: "Conjunto de talheres de alta qualidade.",
-        price: 450,
-        image: "https://postimg.cc/1nqPfWYp",
-        link: "https://mpago.la/1cGm7xN"
-    },
-    {
-        id: 8,
-        name: "Jogo de Toalhas",
-        description: "Toalhas macias e absorventes.",
-        price: 400,
-        image: "https://postimg.cc/N2RgPN6g",
-        link: "https://mpago.la/2VHVete"
-    },
-    {
-        id: 9,
-        name: "Mesinha de Centro",
-        description: "Design elegante para a sala.",
-        price: 500,
-        image: "https://postimg.cc/N2RgPN6g",
-        link: "https://mpago.la/1kHuoFM"
-    },
-    {
-        id: 10,
-        name: "Smart TV",
-        description: "Alta resolução para momentos de lazer.",
-        price: 1350,
-        image: "https://postimg.cc/VrxfgybG",
-        link: "https://mpago.la/14kkSo9"
-    },
-    {
-        id: 11,
-        name: "Conjunto Jogo Americano",
-        description: "Estilo e proteção para sua mesa.",
-        price: 250,
-        image: "https://postimg.cc/VrxfgybG",
-        link: "https://mpago.la/1Ymkwqo"
-    },
-    {
-        id: 12,
-        name: "Faqueiro",
-        description: "Completo com 24 peças em inox.",
-        price: 380,
-        image: "https://postimg.cc/VJKzT0Cb",
-        link: "https://mpago.la/28JZ3PA"
-    },
-    {
-        id: 13,
-        name: "Mesa Lateral",
-        description: "Compacta e prática para sua sala.",
-        price: 620,
-        image: "https://postimg.cc/LnzXysDz",
-        link: "https://mpago.la/2zb6VSX"
-    },
-    {
-        id: 14,
-        name: "Tapete",
-        description: "Grande e aconchegante.",
-        price: 1900,
-        image: "https://postimg.cc/w1jphxGx",
-        link: "https://mpago.la/18s6Zq3"
-    },
-    {
-        id: 15,
-        name: "Mesas de Cabeceira",
-        description: "Conjunto de 2 mesas funcionais.",
-        price: 1850,
-        image: "https://postimg.cc/KkRjQrpz",
-        link: "https://mpago.la/1YT2wKN"
     }
 ];
 
